ddd65e2731115b01719d4d0d62e02077cd6c171a
Rayato159 <rayato159@gmail.com>
2023-09-28 01:10:43 +0700

    -feature :sparkles: response package completed

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,1 +1,17 @@
 package response
+
+import "github.com/labstack/echo/v4"
+
+type (
+	MsgResponse struct {
+		Message string `json:"message"`
+	}
+)
+
+func ErrResponse(c echo.Context, statusCode int, message string) error {
+	return c.JSON(statusCode, &MsgResponse{Message: message})
+}
+
+func SuccessResponse(c echo.Context, statusCode int, data any) error {
+	return c.JSON(statusCode, data)
+}
