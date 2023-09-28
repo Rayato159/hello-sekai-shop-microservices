506fde4de43855138f9f34744407e10f7a6a735d
Rayato159 <rayato159@gmail.com>
2023-09-28 22:46:08 +0700

    -feature :sparkles: player domain completed

--- modules/player/playerEntity.go
+++ modules/player/playerEntity.go
@@ -1,1 +1,37 @@
 package player
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type (
+	Player struct {
+		Id         primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+		Email      string             `json:"email" bson:"email"`
+		Password   string             `json:"password" bson:"password"`
+		Username   string             `json:"username" bson:"username"`
+		CreatedAt  time.Time          `json:"created_at" bson:"created_at"`
+		UpdatedAt  time.Time          `json:"updated_at" bson:"updated_at"`
+		PlayerRole []PlayerRole       `bson:"player_roles"`
+	}
+
+	PlayerRole struct {
+		RoleTitle string `json:"role_title" bson:"role_title"`
+		RoleCode  int    `json:"role_code" bson:"role_code"`
+	}
+
+	PlayerProfileBson struct {
+		Id        primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+		Email     string             `json:"email" bson:"email"`
+		Username  string             `json:"username" bson:"username"`
+		CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+		UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+	}
+
+	PlayerSavingAccount struct {
+		PlayerId string  `json:"player_id" bson:"player_id"`
+		Balance  float64 `json:"balance" bson:"balance"`
+	}
+)
--- modules/player/playerModel.go
+++ modules/player/playerModel.go
@@ -10,4 +10,20 @@ type (
 		CreatedAt time.Time `json:"created_at"`
 		UpdatedAt time.Time `json:"updated_at"`
 	}
+
+	PlayerClaims struct {
+		Id       string `json:"id"`
+		RoleCode int    `json:"role_code"`
+	}
+
+	CreatePlayerReq struct {
+		Email    string `json:"email" form:"email" validate:"required,email,max=255"`
+		Password string `json:"password" form:"password" validate:"required,max=32"`
+		Username string `json:"username" form:"username" validate:"required,max=64"`
+	}
+
+	CreatePlayerTransactionReq struct {
+		PlayerId string  `json:"player_id" validate:"required,max=64"`
+		Amount   float64 `json:"amount" validate:"required"`
+	}
 )
