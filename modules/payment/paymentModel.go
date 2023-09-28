7265f66be6753a2173b406bcd4d5ecc84455f921
Rayato159 <rayato159@gmail.com>
2023-09-28 23:16:51 +0700

    -feature :payment: payment domain completed

--- modules/payment/paymentModel.go
+++ modules/payment/paymentModel.go
@@ -1,1 +1,12 @@
 package payment
+
+type (
+	ItemServiceReq struct {
+		Items []*ItemServiceReqDatum `json:"items" validate:"required"`
+	}
+
+	ItemServiceReqDatum struct {
+		ItemId string  `json:"item_id" validate:"required,max=64"`
+		Price  float64 `json:"price"`
+	}
+)
