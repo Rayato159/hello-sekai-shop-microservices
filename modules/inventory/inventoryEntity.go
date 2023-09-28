0304bad7e009c234efedfca5aecb93c507330b9e
Rayato159 <rayato159@gmail.com>
2023-09-28 23:12:38 +0700

    -feature :sparkles: inventory domain completed

--- modules/inventory/inventoryEntity.go
+++ modules/inventory/inventoryEntity.go
@@ -1,1 +1,9 @@
 package inventory
+
+type (
+	Inventory struct {
+		Id       string `json:"_id" bson:"_id,omitempty"`
+		PlayerId string `json:"player_id" bson:"player_id"`
+		ItemId   string `json:"item_id" bson:"item_id"`
+	}
+)
--- modules/inventory/inventoryModel.go
+++ modules/inventory/inventoryModel.go
@@ -1,1 +1,23 @@
 package inventory
+
+import (
+	"github.com/Rayato159/hello-sekai-shop-tutorial/modules/item"
+	"github.com/Rayato159/hello-sekai-shop-tutorial/modules/models"
+)
+
+type (
+	UpdateInventoryReq struct {
+		PlayerId string `json:"player_id" validate:"required,max=64"`
+		ItemId   string `json:"item_id" validate:"required,max=64"`
+	}
+
+	ItemInInventory struct {
+		InventoryId string `json:"inventory_id"`
+		*item.ItemShowCase
+	}
+
+	PlayerInventory struct {
+		PlayerId string `json:"player_id"`
+		*models.PaginateRes
+	}
+)
