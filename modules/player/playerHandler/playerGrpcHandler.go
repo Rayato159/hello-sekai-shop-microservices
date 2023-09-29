96806cee9e5b7bb624109d3a474dc22df0732102
Rayato159 <rayato159@gmail.com>
2023-09-29 22:31:34 +0700

    -feature :sparkles: player grpc init

--- modules/player/playerHandler/playerGrpcHandler.go
+++ modules/player/playerHandler/playerGrpcHandler.go
@@ -1,13 +1,33 @@
 package playerHandler
 
-import "github.com/Rayato159/hello-sekai-shop-tutorial/modules/player/playerUsecase"
+import (
+	"context"
+
+	playerPb "github.com/Rayato159/hello-sekai-shop-tutorial/modules/player/playerPb"
+	"github.com/Rayato159/hello-sekai-shop-tutorial/modules/player/playerUsecase"
+)
 
 type (
-	playerGrpcHandlerService struct {
+	playerGrpcHandler struct {
 		playerUsecase playerUsecase.PlayerUsecaseService
+		playerPb.UnimplementedPlayerGrpcServiceServer
 	}
 )
 
-func NewPlayerGrpcHandler(playerUsecase playerUsecase.PlayerUsecaseService) *playerGrpcHandlerService {
-	return &playerGrpcHandlerService{playerUsecase}
+func NewPlayerGrpcHandler(playerUsecase playerUsecase.PlayerUsecaseService) *playerGrpcHandler {
+	return &playerGrpcHandler{
+		playerUsecase: playerUsecase,
+	}
+}
+
+func (g *playerGrpcHandler) CredentialSearch(ctx context.Context, req *playerPb.CredentialSearchReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func (g *playerGrpcHandler) FindOnePlayerProfileToRefresh(ctx context.Context, req *playerPb.FindOnePlayerProfileToRefreshReq) (*playerPb.PlayerProfile, error) {
+	return nil, nil
+}
+
+func (g *playerGrpcHandler) GetPlayerSavingAccount(ctx context.Context, req *playerPb.GetPlayerSavingAccountReq) (*playerPb.GetPlayerSavingAccountRes, error) {
+	return nil, nil
 }
