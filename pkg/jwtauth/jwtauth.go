fecc56f8396c7bd6ff8a4cff4cbc08e89f083c01
Rayato159 <rayato159@gmail.com>
2023-10-01 01:20:17 +0700

    -feature :package: apikey generator completed

--- pkg/jwtauth/jwtauth.go
+++ pkg/jwtauth/jwtauth.go
@@ -1,11 +1,14 @@
 package jwtauth
 
 import (
+	"context"
 	"errors"
 	"math"
+	"sync"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"google.golang.org/grpc/metadata"
 )
 
 type (
@@ -112,12 +115,12 @@ func ReloadToken(secret string, expiredAt int64, claims *Claims) string {
 	return obj.SignToken()
 }
 
-func NewApiKey(secret string, expiredAt int64, claims *Claims) AuthFactory {
+func NewApiKey(secret string) AuthFactory {
 	return &apiKey{
 		authConcrete: &authConcrete{
 			Secret: []byte(secret),
 			Claims: &AuthMapClaims{
-				Claims: claims,
+				Claims: &Claims{},
 				RegisteredClaims: jwt.RegisteredClaims{
 					Issuer:    "hellosekai.com",
 					Subject:   "api-key",
@@ -153,4 +156,18 @@ func ParseToken(secret string, tokenString string) (*AuthMapClaims, error) {
 	} else {
 		return nil, errors.New("error: claims type is invalid")
 	}
+}
+
+// Apikey  generator
+var apiKeyInstant string
+var once sync.Once
+
+func SetApiKey(secret string) {
+	once.Do(func() {
+		apiKeyInstant = NewApiKey(secret).SignToken()
+	})
+}
+
+func SetApiKeyInContext(pctx *context.Context) {
+	*pctx = metadata.NewOutgoingContext(*pctx, metadata.Pairs("auth", apiKeyInstant))
 }
