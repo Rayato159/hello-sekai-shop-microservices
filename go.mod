e586ed12162b492d55ef2a347268ca548b62d4eb
Rayato159 <rayato159@gmail.com>
2023-10-01 00:12:59 +0700

    -feature :sparkles: init jwtauth

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.15.4 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
---
+++ pkg/jwtauth/jwtauth.go
@@ -0,0 +1,34 @@
+package jwtauth
+
+import "github.com/golang-jwt/jwt/v5"
+
+type (
+	AuthFactory interface {
+		SignToken() string
+	}
+
+	Claims struct {
+		Id       string `json:"id"`
+		RoleCode int    `json:"role_code"`
+	}
+
+	AuthMapClaims struct {
+		*Claims
+		jwt.RegisteredClaims
+	}
+
+	authConcrete struct {
+		Secret []byte
+		Claims *AuthMapClaims `json:"claims"`
+	}
+
+	accessToken  struct{ *authConcrete }
+	refreshToken struct{ *authConcrete }
+	apiKey       struct{ *authConcrete }
+)
+
+func (a *authConcrete) SignToken() string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, a.Claims)
+	ss, _ := token.SignedString(a.Secret)
+	return ss
+}
