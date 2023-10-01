eab121240215bf8168297afd0ad4327a26340ad8
Rayato159 <rayato159@gmail.com>
2023-10-01 23:47:17 +0700

    -feature :sparkles: jwt authorization completed

--- modules/middleware/middlewareHandler/middlewareHandler.go
+++ modules/middleware/middlewareHandler/middlewareHandler.go
@@ -1,12 +1,19 @@
 package middlewareHandler
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/Rayato159/hello-sekai-shop-tutorial/config"
 	"github.com/Rayato159/hello-sekai-shop-tutorial/modules/middleware/middlewareUsecase"
+	"github.com/Rayato159/hello-sekai-shop-tutorial/pkg/response"
+	"github.com/labstack/echo/v4"
 )
 
 type (
-	MiddlewareHandlerService interface{}
+	MiddlewareHandlerService interface {
+		JwtAuthorization(next echo.HandlerFunc) echo.HandlerFunc
+	}
 
 	middlewareHandler struct {
 		cfg               *config.Config
@@ -18,5 +25,18 @@ func NewMiddlewareHandler(cfg *config.Config, middlewareUsecase middlewareUsecas
 	return &middlewareHandler{
 		cfg:               cfg,
 		middlewareUsecase: middlewareUsecase,
+	}
+}
+
+func (h *middlewareHandler) JwtAuthorization(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		accessToken := strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer ")
+
+		newCtx, err := h.middlewareUsecase.JwtAuthorization(c, h.cfg, accessToken)
+		if err != nil {
+			return response.ErrResponse(c, http.StatusUnauthorized, err.Error())
+		}
+
+		return next(newCtx)
 	}
 }
--- modules/middleware/middlewareUsecase/middlewareUsecase.go
+++ modules/middleware/middlewareUsecase/middlewareUsecase.go
@@ -1,9 +1,16 @@
 package middlewareUsecase
 
-import "github.com/Rayato159/hello-sekai-shop-tutorial/modules/middleware/middlewareRepository"
+import (
+	"github.com/Rayato159/hello-sekai-shop-tutorial/config"
+	"github.com/Rayato159/hello-sekai-shop-tutorial/modules/middleware/middlewareRepository"
+	"github.com/Rayato159/hello-sekai-shop-tutorial/pkg/jwtauth"
+	"github.com/labstack/echo/v4"
+)
 
 type (
-	MiddlewareUsecaseService interface{}
+	MiddlewareUsecaseService interface {
+		JwtAuthorization(c echo.Context, cfg *config.Config, accessToken string) (echo.Context, error)
+	}
 
 	middlewareUsecase struct {
 		middlewareRepository middlewareRepository.MiddlewareRepositoryService
@@ -12,4 +19,22 @@ type (
 
 func NewMiddlewareUsecase(middlewareRepository middlewareRepository.MiddlewareRepositoryService) MiddlewareUsecaseService {
 	return &middlewareUsecase{middlewareRepository}
+}
+
+func (u *middlewareUsecase) JwtAuthorization(c echo.Context, cfg *config.Config, accessToken string) (echo.Context, error) {
+	ctx := c.Request().Context()
+
+	claims, err := jwtauth.ParseToken(cfg.Jwt.AccessSecretKey, accessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := u.middlewareRepository.AccessTokenSearch(ctx, cfg.Grpc.AuthUrl, accessToken); err != nil {
+		return nil, err
+	}
+
+	c.Set("player_id", claims.PlayerId)
+	c.Set("role_code", claims.RoleCode)
+
+	return c, nil
 }
